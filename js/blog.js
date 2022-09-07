76b5e95d3f728f606461ea2d272a4a6f4cb48825
martemo <marte.moslet@hotmail.com>
2022-09-07 22:26:34 +0200

    update blog page

---
+++ js/blog.js
@@ -0,0 +1,17 @@
+const baseUrl = "https://exploreblog.martemoslet.one/wp-json/wp/v2/posts?_embed";
+const blogContainer = document.querySelector(".blog");
+
+async function getBlog(url){
+    const response = await fetch(url);
+    const posts = await response.json();
+    posts.forEach(function(post){
+        blogContainer.innerHTML += `
+        <a href="blogpost.html?id=${post.id}" class="card">
+        <div class="blog-image" style="background-image: url('${post._embedded['wp:featuredmedia']['0'].source_url}')"></div>
+        <h2 class="blog-title">${post.title.rendered}</h2>
+        <div class="blogpost">${post.content.rendered}</div>
+        </a>`;
+    })
+}
+
+getBlog(baseUrl);
