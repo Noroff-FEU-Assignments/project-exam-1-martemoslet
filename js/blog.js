c3532368bee4659d99225267fb4df1a2930e581a
martemo <marte.moslet@hotmail.com>
2022-09-10 15:57:30 +0200

    update blog page

--- js/blog.js
+++ js/blog.js
@@ -1,16 +1,27 @@
-const baseUrl = "https://exploreblog.martemoslet.one/wp-json/wp/v2/posts?_embed";
 const blogContainer = document.querySelector(".blog");
 
-async function getBlog(url){
+const viewmoreButton = document.querySelector("button");
+let page = 0;
+
+
+async function getPosts(url){
     const response = await fetch(url);
     const posts = await response.json();
+    page = page + 1;
+
+    const url = `https://exploreblog.martemoslet.one/wp-json/wp/v2/posts?page=${page}&_embed`;
+
+
     posts.forEach(function(post){
         blogContainer.innerHTML += `
-        <a href="blogpost.html?id=${post.id}" class="card">
-        <div class="blog-image" style="background-image: url('${post._embedded['wp:featuredmedia']['0'].source_url}')"></div>
-        <h2 class="blog-title">${post.title.rendered}</h2>
-        </a>`;
+                                    <a href="blogpost.html?id=${post.id}" class="card">
+                                    <div class="blog-image" style="background-image: url('${post._embedded['wp:featuredmedia']['0'].source_url}')"></div>
+                                    <h2 class="blog-title">${post.title.rendered}</h2>
+                                    </a>`;
+
     })
 }
 
-getBlog(baseUrl);
+viewmoreButton.addEventListener("click", getPosts);
+
+getPosts();
