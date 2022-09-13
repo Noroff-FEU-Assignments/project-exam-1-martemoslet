31e2f75d349c301e756e1a8e95c26b5c13c81116
martemo <marte.moslet@hotmail.com>
2022-09-13 13:00:26 +0200

    Update blog page

--- js/blog.js
+++ js/blog.js
@@ -3,23 +3,29 @@ const blogContainer = document.querySelector(".blog");
 const viewmoreButton = document.querySelector("button");
 let page = 0;
 
+async function getPosts() {
+	page = page + 1;
 
-async function getPosts(url){
-    const response = await fetch(url);
-    const posts = await response.json();
-    page = page + 1;
+	if (page === 1) {
+		blogContainer.innerHTML = "";
+	}
 
-    const url = `https://exploreblog.martemoslet.one/wp-json/wp/v2/posts?page=${page}&_embed`;
+	try {
+		const url = `https://exploreblog.martemoslet.one/wp-json/wp/v2/posts?page=${page}&_embed`;
 
+		const response = await fetch(url);
+		const posts = await response.json();
 
-    posts.forEach(function(post){
-        blogContainer.innerHTML += `
+		posts.forEach(function (post) {
+			blogContainer.innerHTML += `
                                     <a href="blogpost.html?id=${post.id}" class="card">
-                                    <div class="blog-image" style="background-image: url('${post._embedded['wp:featuredmedia']['0'].source_url}')"></div>
+                                    <div class="blog-image" style="background-image: url('${post._embedded["wp:featuredmedia"]["0"].source_url}')"></div>
                                     <h2 class="blog-title">${post.title.rendered}</h2>
                                     </a>`;
-
-    })
+		});
+	} catch (error) {
+		blogContainer.innerHTML = "There was an error retrieving the posts";
+	}
 }
 
 viewmoreButton.addEventListener("click", getPosts);
