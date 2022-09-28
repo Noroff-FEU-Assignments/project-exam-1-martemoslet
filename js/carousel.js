c818c153e2d31da654a4c0fb98eeea168d375d59
martemo <marte.moslet@hotmail.com>
2022-09-28 14:44:10 +0200

    update alt text

--- js/carousel.js
+++ js/carousel.js
@@ -8,10 +8,11 @@ async function getSlider() {
         const response = await fetch(url);
         const post = await response.json();
         const slideImage = post._embedded['wp:featuredmedia'][0].source_url;
+        const slideImageAlt = post._embedded['wp:featuredmedia'][0].alt_text;
 
         slideContainer.innerHTML += `
                                 <a href="blogpost.html?id=${post.id}">
-                                <img id="slideimg" class="slide-image" src="${slideImage}" />
+                                <img id="slideimg" class="slide-image" src="${slideImage}" alt="${slideImageAlt}" />
                                 <h2 class="blog-title">${post.title.rendered}</h2>
                                 </a>
                                 `;
--- js/slidetwo.js
+++ js/slidetwo.js
@@ -8,10 +8,11 @@ async function getSlider() {
         const response = await fetch(urltwo);
         const post = await response.json();
         const slideImage = post._embedded['wp:featuredmedia'][0].source_url;
+        const slideImageAlt = post._embedded['wp:featuredmedia'][0].alt_text;
 
         slideContainerTwo.innerHTML += `
                                 <a href="blogpost.html?id=${post.id}">
-                                <img id="slideimg" class="slide-image" src="${slideImage}" />
+                                <img id="slideimg" class="slide-image" src="${slideImage}" alt="${slideImageAlt}" />
                                 <h2 class="blog-title">${post.title.rendered}</h2>
                                 </a>`;
     }
